c20d69178f125199f809540e2145a7b06e5b7501
Shao0203 <nzsyl0203@gmail.com>
2025-11-03 21:54:27 +0800

    Create common functions of activation functions, gradient, and etc.

---
+++ common/gradient.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+
+# 导数 - 中心差分
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / (2*h)
+
+
+# 导数 - 向前差分
+def numerical_forward(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x)) / h
+
+
+# 偏导数 - 梯度
+def numerical_gradient_basic(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)         # 计算f(x+h)
+        x[idx] = tmp_val - h
+        fxh2 = f(x)         # 计算f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val    # 还原值
+    return grad
+
+
+# 梯度法
+def gradient_descent(f, x, lr=0.01, step_num=100):
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
+
+
+def _numerical_gradient_1d(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val  # 还原值
+
+    return grad
+
+
+def numerical_gradient_2d(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_1d(f, X)
+    else:
+        grad = np.zeros_like(X)
+
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_1d(f, x)
+
+        return grad
+
+
+def numerical_gradient(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val  # 还原值
+        it.iternext()
+
+    return grad
