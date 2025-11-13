6ffe7ae0a98c54b9ec204490a16a965d23c2f2b4
Shao0203 <nzsyl0203@gmail.com>
2025-11-13 16:08:06 +0800

    Review Chapter 3: Completed, build 3 layers network from scratch, using mnist dataset and sample_weights to test the prediction accuracy.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+import pickle
 
 
 # 1. 先定义激活函数:
-# 这里Sigmoid和Relu都能自动处理多维数组，因为用了np.exp, np.maximum
 def identity_function(x):
     return x
 
@@ -15,6 +16,12 @@ def step_single(x):
         return 0
 
 
+def softmax_single(x):
+    x -= np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
+
+
+# 这里step, sigmoid, relu, softmax 都能自动处理多维数组
 def step(x):
     return (x > 0).astype(int)
 
@@ -28,24 +35,55 @@ def relu(x):
 
 
 def softmax(x):
-    return np.exp(x - np.max(x)) / np.sum(np.exp(x - np.max(x)))
+    """
+    keepdims=True 保持维度结构，确保广播正确
+    x = np.array([[1, 2, 3], [4, 5, 6]]) # 形状: (2,3)
+    np.max(x, axis=-1)                   # 形状: (2,)  ← 无法广播 [3, 6] 
+    np.max(x, axis=-1, keepdims=True)    # 形状: (2,1) ← 完美广播 [[3], [6]] 
+    """
+    x -= np.max(x, axis=-1, keepdims=True)
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
 
 
-x = np.array([-2, 0, 3])
-xx = np.array([[-3, 0, 5], [0.5, 0, -0.8]])
 a = np.array([0.3, 2.9, 4.0])
-print(softmax(a))
+print(np.sum(softmax_single(a)))
+aa = np.array([[0.3, 2.9, 4.0], [-0.9, 1, 3], [3, 2, 1]])
+x3 = np.random.randn(2, 3, 4)  # 2个批次，每个3个样本，每个4个类别
+# print(softmax(a))
+# print(softmax(aa))
+# print(x3)
+# print(softmax(x3))
+# print("\n三维输入形状:", softmax(x3).shape)  # (2, 3, 4)
+# print("最后一个维度总和:", np.sum(softmax(x3), axis=-1))
 
-# 2. 再定义神经网络层
+
+# 2. 再定义神经网络层 - todo
 
 
-# 3. 利用层 创建神经网络
+# 3. 利用层 创建神经网络 - todo
 
 
-# 4. 训练 / 测试 神经网络
+# 4. 训练 / 测试 神经网络 - todo
 
 
-"""3 layers network begins
+"""画图比较三种激活函数 Step/Sigmoid/ReLU 
+x = np.arange(-5, 5, 0.1)
+y1 = step(x)
+y2 = sigmoid(x)
+y3 = relu(x)
+plt.plot(x, y1, label='Step')
+plt.plot(x, y2, label='Sigmoid')
+plt.plot(x, y3, label='ReLU')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.ylim(-0.1, 2.1)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.legend()
+plt.show()
+"""
+
+
+"""最简单的流程化3层神经网络的实现 - 向前传播
 X = np.array([1.0, 0.5])
 
 W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
@@ -96,4 +134,71 @@ network = init_network()
 x = np.array([1.0, 0.5])
 y = forward(network, x)
 print(y)
-3 layers network ends"""
+"""
+
+
+"""Mnist数据集 画出第一个数据 - 数字5
+(x_train, t_train), (x_test, t_test) = load_mnist()
+
+img = x_train[0].reshape(28, 28)
+label = t_train[0]
+plt.imshow(img, cmap='gray')
+plt.title(f'Label: {label}')
+plt.axis('off')
+plt.show()    
+"""
+
+
+"""Mnist数据集 使用测试数据 查看训练好的权重参数 的预测准确率
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist()
+    return x_test, t_test
+
+
+def init_network():
+    with open('dataset/sample_weight.pkl', 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)     # 这里softmax只能处理一条数据
+    return y
+
+
+# # 每次预测一条数据 = 1 / 10000
+# x, t = get_data()
+# network = init_network()
+# accuracy_cnt = 0
+# for i in range(len(x)):
+#     y = predict(network, x[i])  # 这里的x[i]是10000条测试数据中的一条
+#     if np.argmax(y) == t[i]:
+#         accuracy_cnt += 1
+# print(f'Accuracy: {accuracy_cnt / len(x):.2%}')     # 0.9352
+
+# # 每次预测一批数据 = 100 / 10000
+x, t = get_data()
+network = init_network()
+accuracy_cnt = 0
+batch_size = 100
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch) # 这里softmax预测的概率是错误的
+    p = np.argmax(y_batch, axis=1)      # 这里获得的预测结果相对位置是正确的
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+print(f'Accuracy: {accuracy_cnt / len(x):.2%} \n')
+# # only look at the first 10 data
+p = np.argmax(predict(network, x[:10]), 1)
+print(p[:10])   # [7 2 1 0 4 1 4 9 6 9]
+print(t[:10])   # [7 2 1 0 4 1 4 9 5 9]
+print(p[:10] == t[:10]) # [ True  True  True  True  True  True  True  True False  True]
+print(np.sum(p[:10] == t[:10])) # 9
+"""
