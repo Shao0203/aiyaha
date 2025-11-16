8773048693207fff80bac6bc458cb39ccb01b9f7
Shao0203 <nzsyl0203@gmail.com>
2025-11-16 20:47:39 +0800

    Review Chapter 5: use computational graph to simulate forward / backward propagation.

--- app.py
+++ app.py
@@ -4,48 +4,6 @@ from dataset.mnist import load_mnist
 import pickle
 
 
-# 公式的直接转换 只能处理输入x是单个值的情况
-def single_step(x):
-    if x > 0:
-        return 1
-    else:
-        return 0
-
-
-def single_softmax(x):
-    x -= np.max(x)
-    return np.exp(x) / np.sum(np.exp(x))
-
-
-def single_cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
-
-
-def single_numerical_gradient(f, x):
-    h = 1e-4
-    grad = np.zeros_like(x)
-
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        x[idx] = tmp_val + h
-        fxh1 = f(x)  # f(x+h)
-        x[idx] = tmp_val - h
-        fxh2 = f(x)  # f(x-h)
-
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val
-
-    return grad
-
-
-def gradient_descent(f, x, lr=0.01, step_num=100):  # 梯度下降
-    for _ in range(step_num):
-        grad = numerical_gradient(f, x)
-        x -= lr * grad
-    return x
-
-
 # 1. 先定义激活函数:
 def identity_function(x):
     return x
@@ -64,12 +22,6 @@ def relu(x):
 
 
 def softmax(x):
-    """
-    keepdims=True 保持维度结构，确保广播正确
-    x = np.array([[1, 2, 3], [4, 5, 6]]) # 形状: (2,3)
-    np.max(x, axis=-1)                   # 形状: (2,)  ← 无法广播 [3, 6] 
-    np.max(x, axis=-1, keepdims=True)    # 形状: (2,1) ← 完美广播 [[3], [6]] 
-    """
     x -= np.max(x, axis=-1, keepdims=True)
     return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
 
@@ -89,10 +41,11 @@ def cross_entropy_error(y, t):
         y = y.reshape(1, y.size)
         t = t.reshape(1, t.size)
     # 如果t是one-hot-vector，转换成标签形式
-    if t.size == y.size:
+    if t.ndim > 1 and t.size == y.size:
         t = t.argmax(axis=-1)
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+    return -np.mean(np.log(y[np.arange(batch_size), t] + 1e-7))
 
 
 # 3. 定义导数计算 数值微分法
@@ -120,134 +73,76 @@ def numerical_gradient(f, x):   # 偏导数
     return grad
 
 
-# 定义简单神经网络
-class SimpleNet:
-    def __init__(self):
-        self.W = np.random.randn(2, 3)
-
-    def predict(self, x):
-        return np.dot(x, self.W)
-
-    def loss(self, x, t):
-        z = self.predict(x)
-        y = softmax(z)
-        loss = cross_entropy_error(y, t)
-        return loss
-
-
-class TwoLayerNet:
-    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
-        self.params = {}
-        self.params['W1'] = weight_init_std * \
-            np.random.randn(input_size, hidden_size)
-        self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * \
-            np.random.randn(hidden_size, output_size)
-        self.params['b2'] = np.zeros(output_size)
-
-    def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-        return y
-
-    def loss(self, x, t):
-        y = self.predict(x)
-        loss = cross_entropy_error(y, t)
-        return loss
-
-    def accuracy(self, x, t):
-        y = self.predict(x)
-        y = np.argmax(y, axis=-1)
-        t = np.argmax(t, axis=-1)
-        accuracy = np.sum(y == t) / x.shape[0]
-        return accuracy
-
-    def numerical_gradient(self, x, t):
-        grads = {}
-        def loss_W(W): return self.loss(x, t)
-        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
-        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
-        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
-        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
-        return grads
-
-    def gradient(self, x, t):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
-        grads = {}
-
-        batch_num = x.shape[0]
-
-        # forward
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-
-        # backward
-        dy = (y - t) / batch_num
-        grads['W2'] = np.dot(z1.T, dy)
-        grads['b2'] = np.sum(dy, axis=0)
-
-        da1 = np.dot(dy, W2.T)
-        dz1 = (1.0 - sigmoid(a1)) * sigmoid(a1) * da1
-        grads['W1'] = np.dot(x.T, dz1)
-        grads['b1'] = np.sum(dz1, axis=0)
-
-        return grads
-
-
-(x_train, t_train), (x_test, t_test) = load_mnist(one_hot_label=True)
-iters_num = 10000
-train_size = x_train.shape[0]
-batch_size = 100
-learning_rate = 0.1
-
-train_loss_list = []
-train_acc_list = []
-test_acc_list = []
-iter_per_epoch = max(train_size / batch_size, 1)
-
-network = TwoLayerNet(784, 50, 10)
-
-for i in range(iters_num):
-    # Mini batch
-    batch_mask = np.random.choice(train_size, batch_size)
-    x_batch = x_train[batch_mask]
-    t_batch = t_train[batch_mask]
-    # calc gradient
-    grad = network.gradient(x_batch, t_batch)
-    # update params
-    for key in ('W1', 'b1', 'W2', 'b2'):
-        network.params[key] -= learning_rate * grad[key]
-    # record loss
-    loss = network.loss(x_batch, t_batch)
-    train_loss_list.append(loss)
-
-    if i % iter_per_epoch == 0:  # one epoch
-        train_acc = network.accuracy(x_train, t_train)
-        test_acc = network.accuracy(x_test, t_test)
-        train_acc_list.append(train_acc)
-        test_acc_list.append(test_acc)
-        print(
-            f'epoch - {i // iter_per_epoch}: train_acc, test_acc | {train_acc:.2%}, {test_acc:.2%}')
-
-markers = {'train': 'o', 'test': 's'}
-x = np.arange(len(train_acc_list))
-plt.plot(x, train_acc_list, label='train acc')
-plt.plot(x, test_acc_list, label='test acc', linestyle='--')
-plt.xlabel("epochs")
-plt.ylabel("accuracy")
-plt.ylim(0, 1.0)
-plt.legend(loc='lower right')
-plt.show()
-
-
 # 4. 定义神经网络层
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+        return dx, dy
+
+
+# buy two apples
+apple = 100
+apple_num = 2
+tax = 1.1
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+# forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+price = mul_tax_layer.forward(apple_price, tax)
+print(apple_price, price)
+# backward
+dprice = 1
+dapple_price, dtax = mul_tax_layer.backward(dprice)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+print(dapple, dapple_num, dtax)
+
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        return x + y
+
+    def backward(self, dout):
+        return dout, dout
+
+
+# buy 2 apples and 3 oranges
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+# forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+print(price)
+# backward
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+print(dapple_num, dapple, dorange, dorange_num, dtax)
+
 
 # 利用层 创建神经网络 - todo
 # 训练 / 测试 神经网络 - todo
@@ -389,6 +284,7 @@ print(p[:10] == t[:10]) # [ True  True  True  True  True  True  True  True False
 print(np.sum(p[:10] == t[:10])) # 9
 """
 
+
 """Mini batch
 (x_train, t_train), (x_test, t_test) = load_mnist(
     flatten=True, normalize=True, one_hot_label=True)
@@ -406,3 +302,173 @@ print(x_batch)
 print('t_batch')
 print(t_batch)
 """
+
+
+"""定义简单神经网络
+class SimpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+        return loss
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+            np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * \
+            np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        loss = cross_entropy_error(y, t)
+        return loss
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=-1)
+        t = np.argmax(t, axis=-1)
+        accuracy = np.sum(y == t) / x.shape[0]
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        grads = {}
+        def loss_W(W): return self.loss(x, t)
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        return grads
+
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+
+        batch_num = x.shape[0]
+
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+
+        da1 = np.dot(dy, W2.T)
+        dz1 = (1.0 - sigmoid(a1)) * sigmoid(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['b1'] = np.sum(dz1, axis=0)
+
+        return grads
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(one_hot_label=True)
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+iter_per_epoch = max(train_size / batch_size, 1)
+
+network = TwoLayerNet(784, 50, 10)
+
+for i in range(iters_num):
+    # Mini batch
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    # calc gradient
+    grad = network.gradient(x_batch, t_batch)
+    # update params
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+    # record loss
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    if i % iter_per_epoch == 0:  # one epoch
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print(
+            f'epoch - {i // iter_per_epoch}: train_acc, test_acc | {train_acc:.2%}, {test_acc:.2%}')
+
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
+"""
+
+
+# 公式的直接转换 只能处理输入x是单个值的情况
+def single_step(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
+
+
+def single_softmax(x):
+    x -= np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
+
+
+def single_cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+
+
+def single_numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)  # f(x+h)
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+
+def gradient_descent(f, x, lr=0.01, step_num=100):  # 梯度下降
+    for _ in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
