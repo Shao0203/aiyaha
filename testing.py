1f1aed2f6c83abacd62df9704177a0702ce290e6
Shao0203 <nzsyl0203@gmail.com>
2025-11-24 20:43:35 +0800

    Code review 01

--- testing.py
+++ testing.py
@@ -1,86 +1,176 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from dataset.mnist import load_mnist
-from common.multi_layer_net_extend import MultiLayerNetExtend
-from common.optimizer import SGD, Adam
-
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
-
-# 减少学习数据
-x_train = x_train[:1000]
-t_train = t_train[:1000]
-
-max_epochs = 20
-train_size = x_train.shape[0]
-batch_size = 100
-learning_rate = 0.01
+from collections import OrderedDict
 
 
-def __train(weight_init_std):
-    bn_network = MultiLayerNetExtend(input_size=784, hidden_size_list=[100, 100, 100, 100, 100], output_size=10,
-                                     weight_init_std=weight_init_std, use_batchnorm=True)
-    network = MultiLayerNetExtend(input_size=784, hidden_size_list=[100, 100, 100, 100, 100], output_size=10,
-                                  weight_init_std=weight_init_std)
-    optimizer = SGD(lr=learning_rate)
-
-    train_acc_list = []
-    bn_train_acc_list = []
-
-    iter_per_epoch = max(train_size / batch_size, 1)
-    epoch_cnt = 0
-
-    for i in range(1000000000):
-        batch_mask = np.random.choice(train_size, batch_size)
-        x_batch = x_train[batch_mask]
-        t_batch = t_train[batch_mask]
-
-        for _network in (bn_network, network):
-            grads = _network.gradient(x_batch, t_batch)
-            optimizer.update(_network.params, grads)
-
-        if i % iter_per_epoch == 0:
-            train_acc = network.accuracy(x_train, t_train)
-            bn_train_acc = bn_network.accuracy(x_train, t_train)
-            train_acc_list.append(train_acc)
-            bn_train_acc_list.append(bn_train_acc)
-
-            print("epoch:" + str(epoch_cnt) + " | " +
-                  str(train_acc) + " - " + str(bn_train_acc))
-
-            epoch_cnt += 1
-            if epoch_cnt >= max_epochs:
-                break
-
-    return train_acc_list, bn_train_acc_list
+# 1. Activation functions
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
-# 3.绘制图形==========
-weight_scale_list = np.logspace(0, -4, num=16)
-x = np.arange(max_epochs)
+def tanh(x):
+    return 2 * sigmoid(2*x) - 1
 
-for i, w in enumerate(weight_scale_list):
-    print("============== " + str(i+1) + "/16" + " ==============")
-    train_acc_list, bn_train_acc_list = __train(w)
 
-    plt.subplot(4, 4, i+1)
-    plt.title("W:" + str(w))
-    if i == 15:
-        plt.plot(x, bn_train_acc_list, label='Batch Normalization', markevery=2)
-        plt.plot(x, train_acc_list, linestyle="--",
-                 label='Normal(without BatchNorm)', markevery=2)
-    else:
-        plt.plot(x, bn_train_acc_list, markevery=2)
-        plt.plot(x, train_acc_list, linestyle="--", markevery=2)
+def relu(x):
+    return np.maximum(0, x)
 
-    plt.ylim(0, 1.0)
-    if i % 4:
-        plt.yticks([])
-    else:
-        plt.ylabel("accuracy")
-    if i < 12:
-        plt.xticks([])
-    else:
-        plt.xlabel("epochs")
-    plt.legend(loc='lower right')
 
-plt.show()
+def softmax(x):
+    x -= np.max(x, axis=-1, keepdims=True)
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+
+
+# 2. Loss function
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        y = y.reshape(1, y.size)
+        t = t.reshape(1, t.size)
+
+    if t.ndim > 1 and t.size == y.size:
+        t = t.argmax(axis=-1)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+# 3. calc gradient
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / (2*h)
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+
+    while not it.finished:
+        idx = it.multi_index
+        tmp = x[idx]
+        x[idx] = tmp + h
+        fxh1 = f(x)
+        x[idx] = tmp - h
+        fxh2 = f(x)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp
+        it.iternext()
+
+    return grad
+
+
+# 4. define network layers
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+class Tanh:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = np.tanh(x)
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1 - self.out ** 2)
+        return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+        self.original_x_shape = None
+
+    def forward(self, x):
+        self.original_x_shape = x.shape
+        self.x = x.reshape(x.shape[0], -1)
+        out = np.dot(self.x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        dx = np.dot(dout, self.W.T)
+        dx = dx.reshape(self.original_x_shape)
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = dout * (self.y - self.t) / batch_size
+        return dx
+
+
+# 5. create neural network
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = np.random.randn(
+            input_size, hidden_size) * weight_init_std
+        self.params['W2'] = np.random.randn(
+            hidden_size, output_size) * weight_init_std
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.lastLayer = SoftmaxWithLoss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        loss = self.lastLayer.forward(y, t)
+        return loss
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
