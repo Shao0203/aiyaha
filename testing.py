26a80c74e1ce6d822c4590d2128a9e115a855b70
Shao0203 <nzsyl0203@gmail.com>
2025-11-25 13:37:24 +0800

    Code review 02 - complete TwoLayerNet and save / load trained_model.pkl with parameters(W, b)

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
+from dataset.mnist import load_mnist
 
 
 # 1. Activation functions
@@ -174,3 +176,131 @@ class TwoLayerNet:
 
     def accuracy(self, x, t):
         y = self.predict(x)
+        y = np.argmax(y, axis=-1)
+        if t.ndim != 1:
+            t = np.argmax(t, axis=-1)
+        accuracy = np.sum(y == t) / x.shape[0]
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        def loss_W(W): return self.loss(x, t)
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        return grads
+
+    def gradient(self, x, t):
+        self.loss(x, t)
+
+        dout = self.lastLayer.backward()
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+        return grads
+
+    def save_model(self, filepath):
+        with open(filepath, 'wb') as f:
+            pickle.dump(self.params, f)
+        print(f'Model params is stored into: {filepath}')
+
+    def load_model(self, filepath):
+        with open(filepath, 'rb') as f:
+            self.params = pickle.load(f)
+        print(f'Model is loaded from {filepath}')
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
+
+# 6. gradient comfirmation
+# (x_train, t_train), (x_test, t_test) = load_mnist(one_hot_label=True)
+# network = TwoLayerNet(784, 50, 10)
+# x_batch = x_train[:3]
+# t_batch = t_train[:3]
+
+# grad_numerical = network.numerical_gradient(x_batch, t_batch)
+# grad_backpropa = network.gradient(x_batch, t_batch)
+# for key in grad_numerical.keys():
+#     diff = np.mean(np.abs(grad_numerical[key] - grad_backpropa[key]))
+#     print(f'{key}: {str(diff)}')
+#     # W1: 3.96350709099096e-10
+#     # b1: 2.171388526952937e-09
+#     # W2: 5.1182810376073035e-09
+#     # b2: 1.3960855106825987e-07
+
+
+# # 7. train and test network
+# (x_train, t_train), (x_test, t_test) = load_mnist(one_hot_label=True)
+# network = TwoLayerNet(784, 50, 10)
+# iters_num = 10000
+# train_size = x_train.shape[0]
+# batch_size = 128
+# iter_per_epoch = max(round(train_size / batch_size), 1)
+# learning_rate = 0.1
+
+# train_loss_list = []
+# train_acc_list = []
+# test_acc_list = []
+
+# for i in range(iters_num):
+#     batch_mask = np.random.choice(train_size, batch_size)
+#     x_batch = x_train[batch_mask]
+#     t_batch = t_train[batch_mask]
+
+#     gradient = network.gradient(x_batch, t_batch)
+
+#     for key in ('W1', 'b1', 'W2', 'b2'):
+#         network.params[key] -= learning_rate * gradient[key]
+
+#     loss = network.loss(x_batch, t_batch)
+#     train_loss_list.append(loss)
+
+#     if i % iter_per_epoch == 0:
+#         train_acc = network.accuracy(x_train, t_train)
+#         test_acc = network.accuracy(x_test, t_test)
+#         train_acc_list.append(train_acc)
+#         test_acc_list.append(test_acc)
+#         print(f'Epoch {i // iter_per_epoch}: {train_acc:.2%}, {test_acc:.2%}')
+
+
+# # 8. draw the loss / accuracy
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+# ax1.plot(np.arange(iters_num), train_loss_list)
+# ax1.set_xlabel('Iterations')
+# ax1.set_ylabel('Loss')
+# ax1.set_title('Train loss value')
+# ax1.grid(True, linestyle='--', alpha=0.5)
+
+# x_acc = np.arange(len(train_acc_list))
+# ax2.plot(x_acc, train_acc_list, label='Train Acc')
+# ax2.plot(x_acc, test_acc_list, label='Test Acc', linestyle='--')
+# ax2.set_xlabel('Epoch')
+# ax2.set_ylabel('Accuracy')
+# ax2.set_title('Train / Test Accuracy')
+# ax2.grid(True, linestyle='--', alpha=0.5)
+# ax2.legend(loc='lower right')
+
+# plt.suptitle('Monitor learning process')
+# plt.tight_layout()
+# plt.show()
+
+# network.save_model('trained_model.pkl')
+
+
+# 9. use the trained_model.pkl
+(x_train, t_train), (x_test, t_test) = load_mnist(one_hot_label=True)
+new_network = TwoLayerNet(784, 50, 10)
+new_network.load_model('trained_model.pkl')
+train_acc = new_network.accuracy(x_train, t_train)
+test_acc = new_network.accuracy(x_test, t_test)
+print(f'train_acc, test_acc | {train_acc:.2%}, {test_acc:.2%}')
+# Model is loaded from trained_model.pkl
+# train_acc, test_acc | 98.16%, 97.11%
