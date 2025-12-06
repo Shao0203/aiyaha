beee56d2e7d919a6d9b27ed9b6bc5e923096763f
Shao0203 <nzsyl0203@gmail.com>
2025-12-06 20:49:46 +0800

    test new functions

--- testing.py
+++ testing.py
@@ -6,12 +6,16 @@ from collections import OrderedDict
 
 
 # 1. Activation & Loss & Gradient
-def relu(x):
-    return np.minimum(0, x)
+def step_func(x):
+    return (x > 0).astype(int)
 
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
 
 
 def tanh(x):
@@ -19,8 +23,12 @@ def tanh(x):
 
 
 def softmax(x):
-    x -= np.max(x, axis=-1, keepdims=True)
+    x = x - np.max(x, axis=-1, keepdims=True)
     return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+
+
+def mean_squre_error(y, t):
+    return 0.5 * np.mean((y - t) ** 2)
 
 
 def cross_entropy_error(y, t):
@@ -29,19 +37,18 @@ def cross_entropy_error(y, t):
         t = t.reshape(1, t.size)
     if t.ndim > 1 and t.size == y.size:
         t = np.argmax(t, axis=-1)
-    return -np.mean(np.log(y[np.arange(y.shape[0]), t] + 1e-7))
+    return np.mean(np.log(y[np.arange(y.shape[0]), t] + 1e-7))
 
 
-def numerical_diff(f, x):
+def num_diff(f, x):
     h = 1e-4
-    return f(x+h) - f(x-h) / (2*h)
+    return (f(x+h) - f(x-h)) / (2*h)
 
 
-def numerical_gradient(f, x):
+def num_grad(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
-
     while not it.finished:
         idx = it.multi_index
         tmp = x[idx]
@@ -55,6 +62,77 @@ def numerical_gradient(f, x):
     return grad
 
 
-# 2. Create Layers
+# Layers
 class Sigmoid:
-    def __
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1 - self.out) * self.out
+        return dx
+
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+        self.original_x_shape = None
+
+    def forward(self, x):
+        self.original_x_shape = x.shape
+        self.x = x.reshape(x.shape[0], -1)
+        out = np.dot(self.x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        dx = np.dot(dout, self.W.T).reshape(self.original_x_shape)
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = dout * (self.y - self.t) / batch_size
+        return dx
+
+
+# 3. Network
+class TwoLayerNet:
+    pass
