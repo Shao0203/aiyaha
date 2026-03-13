4e7105cfcf66cdff226c48c3a080117fae48c451
Shao0203 <nzsyl0203@gmail.com>
2026-03-13 21:03:28 +0800

    Practise building up network from 0

--- testing.py
+++ testing.py
@@ -1,1 +1,182 @@
-print('I am so sick...')
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+
+
+# 1. Activation function
+def identity_function(x):
+    return x
+
+
+def step_function(x):
+    return (x > 0).astype(int)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def tanh(x):
+    return 2 * sigmoid(x*2) - 1
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def softmax(x):
+    x -= np.max(x, axis=-1, keepdims=True)
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+
+
+# 2. Loss functions
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2, axis=-1)
+
+
+def mse(y, t):
+    return 0.5 * np.mean((y - t) ** 2)
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        y = y.reshape(1, y.size)
+        t = t.reshape(1, t.size)
+    if t.ndim > 1 and t.size == y.size:
+        t = np.argmax(t, axis=-1)
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+# 3. differentiation
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / (2*h)
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+        it.iternext()
+
+    return grad
+
+
+# 4. Define network layers: Sigmoid, Relu, Affine, SoftmaxWithLoss
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+        self.original_x_shape = None
+
+    def forward(self, x):
+        self.original_x_shape = x.shape
+        self.x = x.reshape(x.shape[0], -1)
+        out = np.dot(self.x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        dx = np.dot(dout, self.W.T).reshape(self.original_x_shape)
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = dout * (self.y - self.t) / batch_size
+        return dx
+
+
+# 5. Define Neural Network with layers
+class TwoLayerNet:
+    def __init__(self, input, hidden, output, weight_std):
+        # init weight and bias
+        self.params = {}
+        self.params['W1'] = weight_std * np.random.randn(input, hidden)
+        self.params['W2'] = weight_std * np.random.randn(hidden, output)
+        self.params['b1'] = np.zeros(hidden)
+        self.params['b2'] = np.zeros(output)
+        # init layers
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu'] = Relu()
+        self.layers['Affine2'] = Affine(self.parmas['W2'], self.params['b2'])
+        self.lastLayer = SoftmaxWithLoss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        loss = self.lastLayer.forward(y, t)
+        return loss
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=-1)
+        if t.ndim != 1:
+            t = np.argmax(t, axis=-1)
+        accuracy = np.sum(y == t) / x.shape[0]
+        return accuracy
+
+    def numerical_grad(self, x, t):
+        pass
