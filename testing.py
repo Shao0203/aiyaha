7cc04ab2c902c4ab0030378fe832c3991b872d72
Shao0203 <nzsyl0203@gmail.com>
2026-07-10 14:47:51 +0800

    Compare optimizers with loss value and training accuracy.

--- testing.py
+++ testing.py
@@ -1,43 +1,13 @@
-import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 from dataset.mnist import load_mnist
-
-
-# 1. Activation functions
-def identity_func(x):
-    return x
-
-
-def step_func(x):
-    return (x > 0).astype(int)
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def tanh(x):
-    return 2 * sigmoid(2*x) - 1
-
-
-def relu(x):
-    return np.maximum(0, x)
+from common.optimizer import SGD, Momentum, AdaGrad, Adam
 
 
 def softmax(x):
     x -= np.max(x, axis=-1, keepdims=True)
     return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
-
-
-# 2. Loss functions
-def mean_squared_error(y, t):
-    return 0.5 * np.sum((y - t) ** 2, axis=-1)
-
-
-def mse(y, t):
-    return 0.5 * np.mean((y - t) ** 2)
 
 
 def cross_entropy_error(y, t):
@@ -50,13 +20,7 @@ def cross_entropy_error(y, t):
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
 
-# 3. Numerical gradient
-def numerical_diff(f, x):
-    h = 1e-4
-    return (f(x+h) - f(x-h)) / (2 * h)
-
-
-def numerical_grad(f, x):
+def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
@@ -73,7 +37,6 @@ def numerical_grad(f, x):
     return grad
 
 
-# 4. Network layers
 class Relu:
     def __init__(self):
         self.mask = None
@@ -86,22 +49,7 @@ class Relu:
 
     def backward(self, dout):
         dout[self.mask] = 0
-        dx = dout
-        return dx
-
-
-class Sigmoid:
-    def __init__(self):
-        self.out = None
-
-    def forward(self, x):
-        out = 1 / (1 + np.exp(-x))
-        self.out = out
-        return out
-
-    def backward(self, dout):
-        dx = dout * (1.0 - self.out) * self.out
-        return dx
+        return dout
 
 
 class Affine:
@@ -111,10 +59,10 @@ class Affine:
         self.x = None
         self.dW = None
         self.db = None
-        self.original_x_shape = None
+        self.oxs = None
 
     def forward(self, x):
-        self.original_x_shape = x.shape
+        self.oxs = x.shape
         self.x = x.reshape(x.shape[0], -1)
         out = np.dot(self.x, self.W) + self.b
         return out
@@ -122,7 +70,7 @@ class Affine:
     def backward(self, dout):
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
-        dx = np.dot(dout, self.W.T).reshape(self.original_x_shape)
+        dx = np.dot(dout, self.W.T).reshape(self.oxs)
         return dx
 
 
@@ -135,24 +83,20 @@ class SoftmaxWithLoss:
     def forward(self, x, t):
         self.t = t
         self.y = softmax(x)
-        self.loss = cross_entropy_error(self.y, self.t)
-        out = self.loss
-        return out
+        return cross_entropy_error(self.y, self.t)
 
     def backward(self, dout=1):
-        batch_size = self.t.shape[0]
-        dx = dout * (self.y - self.t) / batch_size
+        dx = dout * (self.y - self.t) / self.t.shape[0]
         return dx
 
 
-# 5. Network architecture
 class TwoLayerNet:
-    def __init__(self, input_size, hidden_size, output_size, weight_std_init=0.01):
+    def __init__(self, input, hidden, output, weight_std=0.01):
         self.params = {}
-        self.params['W1'] = weight_std_init * np.random.randn(input_size, hidden_size)
-        self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_std_init * np.random.randn(hidden_size, output_size)
-        self.params['b2'] = np.zeros(output_size)
+        self.params['W1'] = weight_std * np.random.randn(input, hidden)
+        self.params['b1'] = np.zeros(hidden)
+        self.params['W2'] = weight_std * np.random.randn(hidden, output)
+        self.params['b2'] = np.zeros(output)
 
         self.layers = OrderedDict()
         self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
@@ -175,90 +119,81 @@ class TwoLayerNet:
         y = np.argmax(y, axis=-1)
         if t.ndim != 1:
             t = np.argmax(t, axis=-1)
-        acc = np.sum(y == t) / x.shape[0]
-        return acc
+        return np.sum(y == t) / x.shape[0]
 
-    def numerical_gradient(self, x, t):
+    def nume_grad(self, x, t):
         def loss_W(W): return self.loss(x, t)
-        grads = {}
-        grads['W1'] = numerical_grad(loss_W, self.params['W1'])
-        grads['b1'] = numerical_grad(loss_W, self.params['b1'])
-        grads['W2'] = numerical_grad(loss_W, self.params['W2'])
-        grads['b2'] = numerical_grad(loss_W, self.params['b2'])
+        grads = {
+            'W1': numerical_gradient(loss_W, self.params['W1']),
+            'b1': numerical_gradient(loss_W, self.params['b1']),
+            'W2': numerical_gradient(loss_W, self.params['W2']),
+            'b2': numerical_gradient(loss_W, self.params['b2']),
+        }
         return grads
 
-    def gradient(self, x, t):
+    def back_grad(self, x, t):
         self.loss(x, t)
         dout = self.lastLayer.backward()
         layers = list(self.layers.values())
         layers.reverse()
         for layer in layers:
             dout = layer.backward(dout)
-        grads = {}
-        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
-        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+        grads = {
+            'W1': self.layers['Affine1'].dW,
+            'b1': self.layers['Affine1'].db,
+            'W2': self.layers['Affine2'].dW,
+            'b2': self.layers['Affine2'].db,
+        }
         return grads
 
-    def save_model(self, filepath):
-        with open(filepath, 'rb') as f:
-            pickle.dump(self.params, f)
-        print(f'Model is saved to {filepath}.')
 
-    def load_model(self, filepath):
-        with open(filepath, 'rb') as f:
-            self.params = pickle.load(f)
-        print(f'Model is loaded to {filepath}.')
-        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
-        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
-
-
-# 6. Mnist dataset training
-network = TwoLayerNet(784, 50, 10)
 (x_train, t_train), (x_test, t_test) = load_mnist(one_hot_label=True)
-learning_rate = 0.1
-iters_num = 10000
+iters = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 iter_per_epoch = max(1, train_size // batch_size)
+networks, train_loss_list, train_acc_list, test_acc_list = {}, {}, {}, {}
 
-train_acc_list = []
-test_acc_list = []
-train_loss_list = []
 
-for i in range(iters_num):
-    # 1) mini batch
+optimizers = {'SGD': SGD(), 'Momentum': Momentum(), 'AdaGrad': AdaGrad(), 'Adam': Adam()}
+for key in optimizers:
+    networks[key] = TwoLayerNet(784, 50, 10)
+    train_loss_list[key], train_acc_list[key], test_acc_list[key] = [], [], []
+
+for i in range(iters):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    # 2) calculate gradient and update params
-    grads = network.gradient(x_batch, t_batch)
-    for key in grads.keys():
-        network.params[key] -= learning_rate * grads[key]
-    # 3) calculate loss
-    loss = network.loss(x_batch, t_batch)
-    train_loss_list.append(loss)
-    # 4) calculate accuracy
-    if i % iter_per_epoch == 0:
-        train_acc = network.accuracy(x_train, t_train)
-        test_acc = network.accuracy(x_test, t_test)
-        train_acc_list.append(train_acc)
-        test_acc_list.append(test_acc)
-        print(f'Epoch{int(i / iter_per_epoch)}: {train_acc:.4f} | {test_acc:.4f}')
+
+    for key, optimizer in optimizers.items():
+        grads = networks[key].back_grad(x_batch, t_batch)
+        optimizer.update(networks[key].params, grads)
+        loss = networks[key].loss(x_batch, t_batch)
+        train_loss_list[key].append(loss)
+
+        if i % iter_per_epoch == 0:
+            train_acc = networks[key].accuracy(x_train, t_train)
+            test_acc = networks[key].accuracy(x_test, t_test)
+            train_acc_list[key].append(train_acc)
+            test_acc_list[key].append(test_acc)
+            print(f'Epoch {i//iter_per_epoch}: {train_acc:.4f} | {test_acc:.4f} ({key})')
+            if key == 'Adam':
+                print('================================')
 
 
-# 7. Plot the loss value and training results
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
-x_loss = np.arange(len(train_loss_list))
-ax1.plot(x_loss, train_loss_list)
+# ax1 shows loss value while ax2 shows test accuracy
+x_loss = np.arange(len(train_loss_list[key]))
+x_acc = np.arange(len(test_acc_list[key]))
+for key in optimizers:
+    ax1.plot(x_loss, train_loss_list[key], label=key)
+    ax2.plot(x_acc, test_acc_list[key], label=key)
 ax1.set(xlabel='Iteration', ylabel='Loss value', title='Training loss')
 ax1.grid(True, alpha=0.5, ls='--')
-x_acc = np.arange(len(train_acc_list))
-ax2.plot(x_acc, train_acc_list, label='train acc')
-ax2.plot(x_acc, test_acc_list, label='test acc', ls=':')
-ax2.set(xlabel='Epoch', ylabel='Accuracy', title='training accuracy')
-ax2.grid(True, alpha=0.5, ls='--')
+ax1.legend(loc='upper right')
+ax2.set(xlabel='Epoch', ylabel='Accuracy', title='Test Accuracy', ylim=(0, 1))
+ax2.grid(True, alpha=0.5, ls='-')
 ax2.legend(loc='lower right')
-ax2.set_ylim(0, 1.0)
-fig.suptitle('Monitor model training process')
+fig.suptitle('Compare loss and accuracy between optimizers.')
 plt.tight_layout()
 plt.show()
